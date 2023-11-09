87536653a6790c11254e6e29ea60cb9d56cbd5d4
yasser <yasser.fadhl@maxnetds.com>
2023-11-09 17:54:40 +0200

    Blogs Added

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
-const Article = () => {
-  return <div>Article</div>;
+import "./article.css";
+const Article = ({ imgUrl, date, title }) => {
+  return (
+    <div className="gpt3__article">
+      <div className="gpt3__article-image">
+        <img src={imgUrl} alt="blog image" />
+      </div>
+      <div className="gpt3__article-content">
+        <p>{date}</p>
+        <h3>{title}</h3>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
 };
 
 export default Article;
